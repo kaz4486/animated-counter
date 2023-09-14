c3050a782e23d16cefb85ee09364bb80483e5c15
Kamil <kaz4486@gmail.com>
2023-09-14 16:32:58 +0200

    Add styles for paragraph

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import AnimatedNumbers from 'react-animated-numbers';
 
 function App() {
+  const [num, setNum] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='main'>
+        <h1 className='title gradient'>Animated counter</h1>
+        <AnimatedNumbers
+          includeComma
+          animateToNumber={num}
+          fontStyle={{ fontSize: 60 }}
+          locale='en-US'
+          configs={[
+            { mass: 1, tension: 220, friction: 100 },
+            { mass: 1, tension: 180, friction: 130 },
+            { mass: 1, tension: 280, friction: 90 },
+            { mass: 1, tension: 180, friction: 135 },
+            { mass: 1, tension: 260, friction: 100 },
+            // { mass: 1, tension: 210, friction: 180 },
+          ]}
+        ></AnimatedNumbers>
+        <div className='paragraph'>Just try!</div>
+        <div className='buttons_area'>
+          <button
+            className='button'
+            onClick={(num) => setNum((num) => num - 50)}
+          >
+            Decrease
+          </button>
+          <button
+            className='button green'
+            onClick={(num) => setNum((num) => num + 50)}
+          >
+            Increase
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
